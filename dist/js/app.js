265569f6b79ad6e1bcbb312955166402f05b32ba
Brooks Boyd <bro@oks.me>
2017-10-16 14:37:24 -0500

    Create input field and validation checks

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,17 +1,1 @@
-define(['react', 'react-dom', 'Bech32Address'], function (_react, _reactDom, _Bech32Address) {
-  'use strict';
-
-  var _react2 = _interopRequireDefault(_react);
-
-  var _reactDom2 = _interopRequireDefault(_reactDom);
-
-  var _Bech32Address2 = _interopRequireDefault(_Bech32Address);
-
-  function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-      default: obj
-    };
-  }
-
-  _reactDom2.default.render(_react2.default.createElement(_Bech32Address2.default), document.getElementById('container'));
-});
+define(["react","react-dom","Bech32Address"],function(e,t,d){"use strict";function n(e){return e&&e.__esModule?e:{"default":e}}var r=n(e),u=n(t),a=n(d);u["default"].render(r["default"].createElement(a["default"]),document.getElementById("container"))});
--- js/Bech32Address.jsx
+++ js/Bech32Address.jsx
@@ -1,9 +1,93 @@
 import React from 'react';
+import bech32 from 'bech32_ecc';
 
 class Bech32Address extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+      validation: { error: null, pos: [] }
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    let newAddress = e.target.value;
+    let result = bech32(newAddress, ['bc', 'tb']);
+    console.log(result);
+    this.setState(curState => {
+      curState.address = newAddress;
+      curState.validation = result;
+      return curState;
+    });
+  }
+
   render() {
+    let monoStyle = {
+      fontFamily: 'monospace',
+      fontSize: '1rem',
+      width: '50ch',
+      position: 'absolute',
+      padding: '0.2em',
+      margin: 0
+    };
+    if (typeof this.props.style !== 'undefined') {
+      if (typeof this.props.style.fontFamily !== 'undefined') {
+        monoStyle.fontFamily = this.props.style.fontFamily;
+      }
+      if (typeof this.props.style.fontSize !== 'undefined') {
+        monoStyle.fontSize = this.props.style.fontSize;
+      }
+    }
+
+    let underlayStyle = Object.assign({
+      color: 'transparent',
+      top: '0.2em',
+      left: '0.25em'
+    }, monoStyle);
+    let inputStyle = Object.assign({
+      color: '#444',
+      backgroundColor: 'transparent',
+      border: 'solid 1px #CCC'
+    }, monoStyle);
+
+    let underlays = [];
+    let message = false;
+    if (this.state.validation.error !== null) {
+      message = this.state.validation.error;
+    } else {
+      if (this.state.address != '') {
+        inputStyle.backgroundColor = '#CFC';
+      }
+    }
+    this.state.validation.pos.map(errorPos => {
+      underlays.push(
+        <span
+          key={errorPos}
+          style={{
+            position: 'absolute',
+            display: 'inline-block',
+            left: errorPos + 'ch',
+            width: '1ch',
+            background: '#FCC',
+            padding: 0
+          }}
+        >&#x2007;</span>
+      );
+    });
     return (
-      <div>Hello!</div>
+      <div className="address-input-wrapper" style={{ position: 'relative' }}>
+        <div className="address-text-input-wrapper" style={{ height: '1.5em' }}>
+          <div className="address-text-underlay" style={underlayStyle}>{underlays}</div>
+          <input
+            style={inputStyle}
+            type="text"
+            value={this.state.address}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="address-input-errors" style={{ fontSize: '0.8rem', color: '#900' }}>{message}</div>
+      </div>
     );
   }
 }
